65998a029b71e826b6c780aad44650f359c60ae2
prince_beladiya <84062079+PrinceBeladiya@users.noreply.github.com>
2023-01-26 17:41:01 +0530

    final changes added

--- Mode/continous_mode.php
+++ Mode/continous_mode.php
@@ -34,8 +34,8 @@
     </nav>
 
     <div class="container h-100">
-        <div class="row mt-5"">
-            <div class="card style="padding: 15px;">
+        <div class="row mt-5">
+            <div class="card" style="padding: 15px; margin-bottom: 70px;">
                 <form method="POST" class="mb-2">
                     <div class="form-group mb-3">
                         <label for="exampleInputEmail1" class="my-2">Enter data</label>
@@ -58,8 +58,19 @@
                     $f2 = 0;
                     $max = 0;
                     $index = 0; 
+                    $in = 0;
                     $h = 0;
+                    $a_index = 0;
+                    $a = 0;
+                    $fre_sum = 0;
+                    $fidi_sum = 0;
+                    $c = 0;
+                    $f = 0;
                     $abc = array();
+                    $MI = array();
+                    $DI = array();
+                    $Cumi_Fre = array();
+                    $FIDI = array();
 
                     $data = $_POST['mean'];
                     $freq = $_POST['frequency'];
@@ -81,48 +92,161 @@
                                 $index = $x; 
                             }
                         }
+
                         $abc = explode("-", $class[$index]);
-
                         $l = $abc[0];
-                        $h = $abc[1]-$abc[0];
-                        $fm = $max;
-                        $f1 = $freq[$index-1];
-                        $f2 = $freq[$index+1];
-                       
-                        $Mode = ($l+(($fm-$f1)/((2*$fm)-$f1-$f2))*$h);
-                
-                        echo '<label for="exampleInputEmail1" class="my-2">Solution :</label>
-                            <table class="table table-bordered">
-                            <tbody>
-                            <tr>
-                                <th scope="row">X</th>';
+                        $h = $abc[1] - $abc[0];
 
-                            $i = 0;
-                            for ($i = 0; $i < $count1; $i++) {
-                                echo "<td>" . $class[$i] . "</td>";
+                        $last_ele = $count2 - 1;
+
+                        if($index != 0 && $index != $last_ele) {
+                            $fm = $max;
+                            $f1 = $freq[$index-1];
+                            $f2 = $freq[$index+1];
+                           
+                            $Mode = ($l+(($fm-$f1)/((2*$fm)-$f1-$f2))*$h);
+
+                            echo '<label for="exampleInputEmail1" class="my-2">Solution :</label>
+                                  <table class="table table-bordered">
+                                  <tbody>
+                                  <tr>
+                                      <th scope="row">X</th>';
+                          
+                                  $i = 0;
+                                  for ($i = 0; $i < $count1; $i++) {
+                                      echo "<td>" . $class[$i] . "</td>";
+                                  }
+                              
+                            echo '</tr>
+                                    </tbody>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">F</th>';
+                                  
+                                for ($i = 0; $i < $count2; $i++) {
+                                    echo "<td>" . $freq[$i] . "</td>";
+                                }
+                              
+                            echo '</tr>
+                                  </tbody>
+                                  </table>
+                                  <div>Z = L + ((f<sub>m</sub> - f<sub>1</sub>) / (2 * f<sub>m</sub> - f<sub>1</sub> - f<sub>2</sub>)) * h;
+                                  <br><br><label>Z = '.$l.' + ('.$fm.' - '.$f1.') / (2 * '.$fm.' - '.$f1.' - '.$f2.') * '.$h.'</label></div>';
+                            echo '<div style="margin-top: 13px;">
+                                    <label class="mt-2">';
+                            echo "Mode is : " . $Mode;
+                            echo '</label>
+                                  </div>
+                                  </label>';
+                        } else {
+                            for ($x = 0; $x < $count1; $x++) {
+                                $fre_sum = (int)$fre_sum + (int)$freq[$x];
                             }
 
-                        echo '</tr>
-                                </tbody>
-                                <tbody>
-                                    <tr>
-                                    <th scope="row">F</th>';
+
+                            for ($y = 0; $y < $count2; $y++) {
+
+                                if ($y == 0) {
+                                    $Cumi_Fre[$y] = (int)$freq[$y];
+                                } else {
+                                    $Cumi_Fre[$y] = $Cumi_Fre[$y - 1] + (int)$freq[$y];
+                                }
+                            }
+
+
+                            if(($count2 % 2) != 0) {
+                                $a_index = $count2 / 2;
+                                $a_index = ((int)$a_index);
+                            } else {
+                                $a_index = $count2 / 2;
+                            }
 
                             for ($i = 0; $i < $count2; $i++) {
-                                echo "<td>" . $freq[$i] . "</td>";
+                                $mi = explode("-", $class[$i]);
+                                $MI[$i] = ($mi[0] + $mi[1]) / 2;
+
+                                if($i == $a_index) {
+                                    $a = $MI[$i];
+                                }
                             }
 
-                        echo '</tr>
-                            </tbody>
-                            </table>
-                            <div>Z = L + ((f<sub>m</sub> - f<sub>1</sub>) / (2 * f<sub>m</sub> - f<sub>1</sub> - f<sub>2</sub>)) * h;
-                            <br><br><label>Z = '.$l.' + ('.$fm.' - '.$f1.') / (2 * '.$fm.' - '.$f1.' - '.$f2.') * '.$h.'</label></div>';
-                        echo '<div style="margin-top: 13px;">
-                                <label class="mt-2">';
-                        echo "Mode is : " . $Mode;
-                        echo '</label>
-                            </div>
-                            </label>';
+
+                            for ($i = 0; $i < $count2; $i++) {
+                                $DI[$i] = ($MI[$i] - $a) / $h;
+                            }
+
+
+                            for ($i = 0; $i < $count2; $i++) {
+                                $FIDI[$i] = $freq[$i] * $DI[$i];
+                            }
+
+
+                            for ($x = 0; $x < $count1; $x++) {
+                                $fidi_sum = (int)$fidi_sum + (int)$FIDI[$x];
+                            }
+
+
+                            for ($x = 0; $x < $count1; $x++) {
+                            
+                                if (($fre_sum / 2) < $Cumi_Fre[$x]) {
+                                    $abc = explode("-", $class[$x]);
+                                    $l = $abc[0];
+                                    $c = $Cumi_Fre[$x-1];
+                                    $f = $FIDI[$x];
+                                    $in = $x;
+                                    break;
+                                }
+                            }
+
+                            echo '<label for="exampleInputEmail1" class="my-2">Solution :</label>
+                                  <table class="table">
+                                  <thead>
+                                    <tr>
+                                      <th scope="col">Class</th>
+                                      <th scope="col">Frequency</th>
+                                      <th scope="col">mi</th>
+                                      <th scope="col">di</th>
+                                      <th scope="col">fidi</th>
+                                      <th scope="col">Cum_Fre</th>
+                                    </tr>
+                                  </thead>
+                                  <tbody class="table-group-divider">';
+                                  $i = 0;
+                                  for ($i = 0; $i < $count1; $i++) {
+                                    echo "<tr>";
+                                    echo "<td>" . $class[$i] . "</td>";
+                                    echo "<td>" . $freq[$i] . "</td>";
+                                    echo "<td>" . $MI[$i] . "</td>";
+                                    echo "<td>" . $DI[$i] . "</td>";
+                                    echo "<td>" . $FIDI[$i] . "</td>";
+                                    echo "<td>" . $Cumi_Fre[$i] . "</td>";
+                                    echo "</tr>";
+                                  }
+                                  echo '<tr>
+                                    <td></td>
+                                    <td>N = ';
+                                  echo $fre_sum;
+                                  echo '</td>
+
+                                  <td></td>
+                                  <td></td>
+                                  <td>';
+                                  echo "&#931;fidi = " . $fidi_sum;
+                                  echo '</td></tbody>
+                                </table>
+                                <div>x&#772 = a + <u>&#931fidi</u> * h<br />&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160N</div>
+                                    <label class="mt-2">
+                                    <div>x&#772 = '.$a.' + <u>'.$fidi_sum.'</u> * '.$h.'<br />&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160&#160;'.$fre_sum.'</div>';
+                                  
+                                  echo '<div>x&#772 = '. ($a + (($fidi_sum / $fre_sum) * $h)) .'</div><hr>';
+                                  echo '<div>N / 2 = '. ($fre_sum / 2) .'</div><hr>
+                                        <div>CLASS = '. $class[$in] .'</div><br>
+                                        <div>M = L + ( ( (n / 2) - c) * h) / f
+                                        <br><br><label>M = '.$l.' + ( ( ('.$fre_sum.' / 2) - '.$c.') * '.$h.') / '.$f.'</label></div>
+                                        <br><div>M = '. $l + ((($fre_sum / 2) - $c) / $f) * $h .'</div><hr>
+                                        <div>Z = 3M - 2x&#772;
+                                        <br><br><label>Z = '. ((3 * ($l + ((($fre_sum / 2) - $c) / $f) * $h)) - (2 * ($a + (($fidi_sum / $fre_sum) * $h)))) .'</label></div>';
+                        }
                         }
                     } else {
                         $Mode = "";
